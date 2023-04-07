959262af0946e961abb262e83e2cabb03dc85e16
akki2825 <akhilesh.rocking@gmail.com>
2023-04-07 17:50:35 +0200

    cleaning

--- src/create_training_data.py
+++ src/create_training_data.py
@@ -1,25 +1,36 @@
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 
 
 if __name__ == "__main__":
 
 
-    data = pd.read_csv("../data/IPA_NIKL_lC+Obstruent.csv")
+    data = pd.read_csv("/home/akhilesh/personal/sigmorphon2023/data/all_NIKL.csv").fillna("")
 
-    ipa_morph_r = data["IPA_Morphology_R"].tolist()
+    morph_r = data["Morphology_R"].tolist()
 
-    simplify_del_obstruent = data["simplify_delete_obstruent"].tolist()
+    # simplify_del_obstruent = data["simplify_delete_obstruent"].tolist()
 
-    simplify_del_lateral = data["simplify_delete_lateral"].tolist()
+    # simplify_del_lateral = data["simplify_delete_lateral"].tolist()
 
-    ipa_prod_r = data["IPA_Production_R"].tolist()
+    prod_r = data["Production_R"].tolist()
 
     _input = []
     _output = []
 
-    for morph, sdo, sdl, prod in zip(ipa_morph_r, simplify_del_obstruent, simplify_del_lateral, ipa_prod_r):
+    for morph, prod in zip(morph_r, prod_r):
         morph = " ".join(str(item) for item in morph)
-        _input.append(morph + " " + str(sdo) + " " + str(sdl))
+        _input.append(morph)
         prod = " ".join(str(i) for i in prod)
         _output.append(prod)
+
+    with open("/home/akhilesh/personal/sigmorphon2023/data/neural/baseline/kr.input", "w+"):
+        for item in _input:
+            f.write(item + "\n")
+
+    with open("/home/akhilesh/personal/sigmorphon2023/data/neural/baseline/kr.output", "w+"):
+        for item in _output:
+            f.write(item + "\n")
+
+    X_train, X_test, y_train, y_test = train_test_split(_input, _output, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.125, random_state=1)
